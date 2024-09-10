6175df662cdb630c0ec94f2a254e02244cbab822
孙嘉城 <sunjiacheng@catarc.ac.cn>
2024-09-11 01:11:50 +0800

    style(all):v0.1.3 change file style.

--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -21,10 +21,11 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "iwdg.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,11 +47,202 @@
 /* USER CODE BEGIN Variables */
 
 /* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 64 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+/* Definitions for myTask_WDog */
+osThreadId_t myTask_WDogHandle;
+const osThreadAttr_t myTask_WDog_attributes = {
+  .name = "myTask_WDog",
+  .stack_size = 64 * 4,
+  .priority = (osPriority_t) osPriorityRealtime,
+};
+/* Definitions for myTask_232Rx */
+osThreadId_t myTask_232RxHandle;
+const osThreadAttr_t myTask_232Rx_attributes = {
+  .name = "myTask_232Rx",
+  .stack_size = 64 * 4,
+  .priority = (osPriority_t) osPriorityHigh,
+};
+/* Definitions for myTask_485C1Tx */
+osThreadId_t myTask_485C1TxHandle;
+const osThreadAttr_t myTask_485C1Tx_attributes = {
+  .name = "myTask_485C1Tx",
+  .stack_size = 64 * 4,
+  .priority = (osPriority_t) osPriorityBelowNormal,
+};
+/* Definitions for myTask_WorkFlow */
+osThreadId_t myTask_WorkFlowHandle;
+const osThreadAttr_t myTask_WorkFlow_attributes = {
+  .name = "myTask_WorkFlow",
+  .stack_size = 64 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 
 /* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void StartTask_WDog(void *argument);
+void StartTask_232Rx(void *argument);
+void StartTask_485C1Tx(void *argument);
+void StartTask_WorkFlow(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of myTask_WDog */
+  myTask_WDogHandle = osThreadNew(StartTask_WDog, NULL, &myTask_WDog_attributes);
+
+  /* creation of myTask_232Rx */
+  myTask_232RxHandle = osThreadNew(StartTask_232Rx, NULL, &myTask_232Rx_attributes);
+
+  /* creation of myTask_485C1Tx */
+  myTask_485C1TxHandle = osThreadNew(StartTask_485C1Tx, NULL, &myTask_485C1Tx_attributes);
+
+  /* creation of myTask_WorkFlow */
+  myTask_WorkFlowHandle = osThreadNew(StartTask_WorkFlow, NULL, &myTask_WorkFlow_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(500);
+    HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_StartTask_WDog */
+/**
+* @brief Function implementing the myTask_WDog thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask_WDog */
+void StartTask_WDog(void *argument)
+{
+  /* USER CODE BEGIN StartTask_WDog */
+	TickType_t xLastWakeTime;
+  /* Infinite loop */
+  for(;;)
+  {
+    xLastWakeTime = osKernelGetTickCount();
+    osDelayUntil(xLastWakeTime + 300);
+    HAL_IWDG_Refresh(&hiwdg);
+  }
+  /* USER CODE END StartTask_WDog */
+}
+
+/* USER CODE BEGIN Header_StartTask_232Rx */
+/**
+* @brief Function implementing the myTask_232Rx thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask_232Rx */
+void StartTask_232Rx(void *argument)
+{
+  /* USER CODE BEGIN StartTask_232Rx */
+	TickType_t xLastWakeTime;
+  /* Infinite loop */
+  for(;;)
+  {
+    xLastWakeTime = osKernelGetTickCount();
+    osDelayUntil(xLastWakeTime + 5);
+  }
+  /* USER CODE END StartTask_232Rx */
+}
+
+/* USER CODE BEGIN Header_StartTask_485C1Tx */
+/**
+* @brief Function implementing the myTask_485C1Tx thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask_485C1Tx */
+void StartTask_485C1Tx(void *argument)
+{
+  /* USER CODE BEGIN StartTask_485C1Tx */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartTask_485C1Tx */
+}
+
+/* USER CODE BEGIN Header_StartTask_WorkFlow */
+/**
+* @brief Function implementing the myTask_WorkFlow thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_StartTask_WorkFlow */
+void StartTask_WorkFlow(void *argument)
+{
+  /* USER CODE BEGIN StartTask_WorkFlow */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartTask_WorkFlow */
+}
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -55,8 +55,13 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern CAN_HandleTypeDef hcan;
 extern DMA_HandleTypeDef hdma_usart1_rx;
+extern DMA_HandleTypeDef hdma_usart1_tx;
+extern DMA_HandleTypeDef hdma_usart2_rx;
+extern DMA_HandleTypeDef hdma_usart2_tx;
 extern DMA_HandleTypeDef hdma_usart3_tx;
+extern DMA_HandleTypeDef hdma_usart3_rx;
 extern UART_HandleTypeDef huart1;
 extern UART_HandleTypeDef huart2;
 extern UART_HandleTypeDef huart3;
@@ -179,6 +184,34 @@ void DMA1_Channel2_IRQHandler(void)
 }
 
 /**
+  * @brief This function handles DMA1 channel3 global interrupt.
+  */
+void DMA1_Channel3_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel3_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel3_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart3_rx);
+  /* USER CODE BEGIN DMA1_Channel3_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 channel4 global interrupt.
+  */
+void DMA1_Channel4_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel4_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel4_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart1_tx);
+  /* USER CODE BEGIN DMA1_Channel4_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel4_IRQn 1 */
+}
+
+/**
   * @brief This function handles DMA1 channel5 global interrupt.
   */
 void DMA1_Channel5_IRQHandler(void)
@@ -190,6 +223,62 @@ void DMA1_Channel5_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Channel5_IRQn 1 */
 
   /* USER CODE END DMA1_Channel5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 channel6 global interrupt.
+  */
+void DMA1_Channel6_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel6_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel6_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart2_rx);
+  /* USER CODE BEGIN DMA1_Channel6_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel6_IRQn 1 */
+}
+
+/**
+  * @brief This function handles DMA1 channel7 global interrupt.
+  */
+void DMA1_Channel7_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel7_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel7_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart2_tx);
+  /* USER CODE BEGIN DMA1_Channel7_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel7_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USB high priority or CAN TX interrupts.
+  */
+void USB_HP_CAN1_TX_IRQHandler(void)
+{
+  /* USER CODE BEGIN USB_HP_CAN1_TX_IRQn 0 */
+
+  /* USER CODE END USB_HP_CAN1_TX_IRQn 0 */
+  HAL_CAN_IRQHandler(&hcan);
+  /* USER CODE BEGIN USB_HP_CAN1_TX_IRQn 1 */
+
+  /* USER CODE END USB_HP_CAN1_TX_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USB low priority or CAN RX0 interrupts.
+  */
+void USB_LP_CAN1_RX0_IRQHandler(void)
+{
+  /* USER CODE BEGIN USB_LP_CAN1_RX0_IRQn 0 */
+
+  /* USER CODE END USB_LP_CAN1_RX0_IRQn 0 */
+  HAL_CAN_IRQHandler(&hcan);
+  /* USER CODE BEGIN USB_LP_CAN1_RX0_IRQn 1 */
+
+  /* USER CODE END USB_LP_CAN1_RX0_IRQn 1 */
 }
 
 /**
